845d83059c9685acac5bc044f496e141d633dab7
Deepanshu Mishra <d4deepanshu723@gmail.com>
2026-06-11 18:07:24 +0530

    prepare:clouflare wrk deploy

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,8 @@
-import { FastMCP } from "fastmcp";
+import { EdgeFastMCP } from "fastmcp/edge";
 
-export const server = new FastMCP({
+export const server = new EdgeFastMCP({
   name: "job-board-mcp",
   version: "1.0.0"
 });
 
-
-server.start({
-  transportType: "httpStream",
-  httpStream: {
-    port: 8080
-  }
-})
+export default server;
